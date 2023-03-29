fb070116d8e71ae9bb30a389eabd386bf8eb2dcf
Zhambulova Tabigat <zhambulovat@gmail.com>
2023-03-29 15:24:09 +0600

    girst commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/scenes/Navbar/";
+
+function App() {
+  return (
+    <div className="app bg-gray-20 bg-gray-50"> 
+    <Navbar/>
+     </div>
+  )
+}
+ 
+export default App;
+
+ //tsx-component functions
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+
+
+type Props = {};
+const Navbar = (props: Props) => {
+    
+    const flexBeyween = "flex items-center justify-between"
+  
+    return  <nav>
+        <div
+        className = {
+            
+        }>
+            
+        </div>
+    </nav>
+  
+}
+
+export default Navbar
